5a8c9ea4a0f3a337a2c4a7ea88576dfc49831bdd
Gianluca Elia <34285663+gianlukk994@users.noreply.github.com>
2023-11-16 17:49:19 +0100

    Create skill model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_16_090257) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_16_111145) do
   create_table "employment_experiences", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -39,6 +39,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_16_090257) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_resumes_on_user_id"
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.integer "category"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
