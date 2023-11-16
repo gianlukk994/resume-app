6bcc646f9203ef6e57f30ee90fc63ba514c889ba
Gianluca Elia <34285663+gianlukk994@users.noreply.github.com>
2023-11-16 09:58:20 +0100

    Add annotations on models

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: resumes
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  summary    :string
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Resume < ApplicationRecord
   belongs_to :user
   has_many :employment_experiences, through: :user
--- spec/models/employment_experience_spec.rb
+++ spec/models/employment_experience_spec.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: employment_experiences
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :string
+#  start_date  :datetime
+#  end_date    :datetime
+#  user_id     :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe EmploymentExperience, type: :model do
